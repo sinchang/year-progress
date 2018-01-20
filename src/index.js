90ca40c67259135f48dcdd503b2704289a491218
sinchang <sinchangwen@gmail.com>
2018-01-20 15:32:59 +0800

    feat: support leapYear

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const isDate = require('lodash.isdate')
 
-export default function (date) {
+module.exports = function (date) {
   if (date) {
     if (!isDate(date)) {
       throw new Error('incorrent date')
@@ -11,10 +11,13 @@ export default function (date) {
 
   // code from: https://github.com/nicomf1982/year-progress-bar
   const initialDate = new Date(date.getFullYear(), 0, 1)
-  const percent = Math.floor((((date - initialDate) / (1000 * 60 * 60 * 24)) * 100) / 365)
+  const isLeapYear = year => {
+    return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)
+  }
+  const percent = Math.floor((((date - initialDate) / (1000 * 60 * 60 * 24)) * 100) / (isLeapYear(date.getFullYear()) ? 366 : 365))
   let yearBar = ''
 
-  for (let i = 5; i <= 100; i += 6) {
+  for (let i = 5; i <= 100; i += 5) {
     yearBar = (i < percent) ? yearBar + '▓' : yearBar + '░'
   }
 
